1a4ae2915629050566031cb34435494a6217fad0
TheSainEyereg <oleglogwinow@gmail.com>
2022-11-16 23:24:51 +0300

    Added support for unavailable tracks
    + Updated revision system

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const {YMAuth, targetPlaylist} = require("./config.json");
 
 const ymApi = new YMApi();
 
+let lastRevision = 0;
 async function waitForRevision(action) {
 	try {
-		return await action(1);
+		return await action(lastRevision+1);
 	} catch (e) {
 		if (e.name === "wrong-revision") {
-			return action(parseInt(e.message.split("actual revision: ")[1]));
+			lastRevision = parseInt(e.message.split("actual revision: ")[1])
+			return action(lastRevision);
 		} else throw e;
 	}
 }
@@ -24,26 +26,30 @@ async function waitForRevision(action) {
 
 	console.log("Checking for extra tracks...");
 	const toRemove = Array.from(targetTracks)
-		.filter(tt => !likedTracks.find(lt => lt.available && (lt.id === tt.id) && (lt.albums[0].id === tt.albums[0].id))); // lt -- LikedTrack; tt -- TargetTrack
+		.filter(tt => !likedTracks.find(lt => (lt.id === tt.id) && (!lt.available || (lt.albums[0].id === tt.albums[0].id)))); // lt -- LikedTrack; tt -- TargetTrack
+	
 	let removedCount = 0;
 	for (const track of toRemove) {
-		if (!track.available) continue;
-
-		await waitForRevision(r => ymApi.removeTracksFromPlaylist(
-			""+targetPlaylist,
-			[{id: track.id, albumId: track.albums[0].id}],
-			r,
-			{from: targetTracks.indexOf(track)-removedCount, to: targetTracks.indexOf(track)+1-removedCount}
-		));
-		removedCount++
+		try {
+			await waitForRevision(r => ymApi.removeTracksFromPlaylist(
+				""+targetPlaylist,
+				//track.available ? [{id: track.id, albumId: track.albums[0].id}] : undefined,
+				undefined, // You don't have to specify the track ID
+				r,
+				{from: targetTracks.indexOf(track)-removedCount, to: targetTracks.indexOf(track)+1-removedCount}
+			));
+			removedCount++
+		} catch (e) {
+			console.log(`Can't delete track "${track.artists?.map(a => a.name).join(", ")} - ${track.title}" on position ${targetTracks.indexOf(track)-removedCount} (${e.name})`)
+		}
 	}
 	console.log(`Removed ${toRemove.length} track${toRemove.length == 1 ? "s" : ""}`);
 
 	
 	console.log("Checking for missing tracks...");
 	const toAddArray = Array.from(likedTracks)
-		.filter(lt => lt.available && !targetTracks.find(tt => (tt.id === lt.id) && (tt.albums[0].id === lt.albums[0].id)))
-		.map(t => ({id: t.id, albumId: t.albums[0].id})); // {"id":"50501070","albumId":6977259}
+		.filter(lt => !targetTracks.find(tt => (tt.id === lt.id) && (!tt.available || (tt.albums[0].id === lt.albums[0].id))))
+		.map(t => ({id: t.id, albumId: t.available ? t.albums[0].id : undefined})); // {"id":"50501070","albumId":6977259}
 	await waitForRevision(r => ymApi.addTracksToPlaylist(""+targetPlaylist, toAddArray, r));
 	console.log(`Added ${toAddArray.length} track${toAddArray.length == 1 ? "s" : ""}`);
 
